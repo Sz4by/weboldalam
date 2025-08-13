3e99dc9c01f8d62ed536735880a3f38ebf314ec4
Szaby <szaby.korodi@yahoo.com>
2025-08-13 23:03:23 +0300

    Update extra.js

--- public/szaby/extra.js
+++ public/szaby/extra.js
@@ -28,7 +28,6 @@ function reportBadActivity(reason) {
     };
 
     // Küldd el a jelentést a szervernek (itt az API endpoint-ot hívhatod meg)
-    fetch('/kill', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -49,4 +48,5 @@ document.getElementById("acceptBtn").onclick = function() {
     document.getElementById("blockModal").style.display = "none"; // Modal eltüntetése
     document.getElementById("audio").play(); // Zene elindítása
 };
+
 
