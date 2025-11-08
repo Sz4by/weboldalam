5d193d376179efdeb5b0e2719fbe9b35ccc04739
Szaby <szaby.korodi@yahoo.com>
2025-11-08 18:26:12 +0200

    Update script.js

--- public/szaby/script.js
+++ public/szaby/script.js
@@ -147,7 +147,7 @@ loadSong(current);
 // ---- DISCORD STATUS FROM RENDER API ----
 async function fetchDiscordStatus() {
   try {
-    const response = await fetch('http://217.154.239.23:14563/api/status');
+    const response = await fetch('https://test-status-1.onrender.com/status');
     const data = await response.json();
     updateDiscordStatus(data);
   } catch (error) {
